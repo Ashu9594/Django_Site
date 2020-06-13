77ed58b5f4edd52e943c19b3a542cd150d17e046
= <=>
2020-06-13 19:08:32 +0530

    Database changes

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class Category:
-    id : int
-    name : str
-    price : float
-    desc : str
-    img : str
+class Category(models.Model):
+    # id : int by default django provides id 
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    desc = models.TextField()
+    img = models.ImageField(upload_to="pics")
+
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -5,23 +5,23 @@ from .models import Category
 # Create your views here.
 def home(request):
 
-    cat1 = Category()
-    cat1.name = 'Adidas'
-    cat1.price = 12500
-    cat1.desc = 'this is the best footwear brand'
-    cat1.img = 'footwear.jpg'
+    # cat1 = Category()
+    # cat1.name = 'Adidas'
+    # cat1.price = 12500
+    # cat1.desc = 'this is the best footwear brand'
+    # cat1.img = 'footwear.jpg'
 
-    cat2 = Category()
-    cat2.name = 'Samsung M10'
-    cat2.price = 25000
-    cat2.desc = ' this is the desc for samsung'
-    cat2.img = 'samsung.jpeg'
+    # cat2 = Category()
+    # cat2.name = 'Samsung M10'
+    # cat2.price = 25000
+    # cat2.desc = ' this is the desc for samsung'
+    # cat2.img = 'samsung.jpeg'
     
-    cat3 = Category()
-    cat3.name = 'Dell'
-    cat3.price = 500000
-    cat3.desc = 'this is dell laptop'
-    cat3.img = 'dell.jpeg'
+    # cat3 = Category()
+    # cat3.name = 'Dell'
+    # cat3.price = 500000
+    # cat3.desc = 'this is dell laptop'
+    # cat3.img = 'dell.jpeg'
     
 
     # cat4 = Category() 
@@ -30,6 +30,8 @@ def home(request):
     # cat4.desc = 'this is dell laptop'
     # cat4.img = 'dell.jpeg'
     
-    cat = [cat1,cat2,cat3]
+    # cat = [cat1,cat2,cat3]
+
+    cat = Category.objects.all()
     return render(request, 'home.html',{'cat':cat})
     
